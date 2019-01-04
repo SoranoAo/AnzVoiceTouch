7a0b08ac61d0ca620b891ba0753c74dbd10c6673
Sotano Ao <sora.aoi.clover@gmail.com>
2019-01-04 18:48:49 +0900

    初期コミット作りかけ

---
+++ app/src/main/java/anzutouchvoice/honeycomb_heart/amebaownd/com/anzutouchvoice/Acitivity/GridAdapter/VoiceAdapter/VoiceAdapterItem.java
@@ -0,0 +1,15 @@
+package anzutouchvoice.honeycomb_heart.amebaownd.com.anzutouchvoice.Acitivity.GridAdapter.VoiceAdapter;
+
+/**
+ * 一つのボイスボタンのアイテムを示すクラス
+ */
+public class VoiceAdapterItem {
+
+
+    private String Titile;
+    
+
+
+
+
+}
